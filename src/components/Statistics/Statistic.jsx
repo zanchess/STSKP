eb326f796ba7e1fcc74de96203fd059a9375f8dd
Alexandr <zanchesssss@gmail.com>
2021-01-20 23:17:09 +0300

    feat: add some graphics in statistic page

--- src/components/Statistics/Statistic.jsx
+++ src/components/Statistics/Statistic.jsx
@@ -22,6 +22,26 @@ const StatisticLongTerm = (props) => {
     }, 0);
   }
 
+  if (props.statsInfo.swimming) {
+    dates.swimming = props.statsInfo.swimming.map((item) => item.date);
+    numOfWords.swimming = props.statsInfo.swimming.map((item) => item.words);
+    sumOfWords.swimming = [];
+    numOfWords.swimming.reduce((acc, item) => {
+      sumOfWords.swimming.push(acc + item);
+      return (acc + item);
+    }, 0);
+  }
+
+  if (props.statsInfo.calories) {
+    dates.calories = props.statsInfo.swimming.map((item) => item.date);
+    numOfWords.calories = props.statsInfo.swimming.map((item) => item.words);
+    sumOfWords.calories = [];
+    numOfWords.calories.reduce((acc, item) => {
+      sumOfWords.calories.push(acc + item);
+      return (acc + item);
+    }, 0);
+  }
+
   /*   const left = sumOfWords.map((it) => 3600 - it);
   const percent = sumOfWords.map((it) => ((100 * it) / 3600).toFixed(1)); */
 
@@ -53,14 +73,14 @@ const StatisticLongTerm = (props) => {
   };
 
   const swimmingData = {
-    labels: dates,
+    labels: dates.swimming,
     datasets: [
       {
         label: 'Плаванье',
         fill: true,
         lineTension: 0.1,
-        backgroundColor: 'rgba(37,95,82,0.47)',
-        borderColor: 'rgba(37,95,82,0.47)',
+        backgroundColor: 'rgba(77,77, 77,0.47)',
+        borderColor: 'rgba(77,77, 77,0.47)',
         borderCapStyle: 'butt',
         borderDash: [],
         borderDashOffset: 0.0,
@@ -74,7 +94,34 @@ const StatisticLongTerm = (props) => {
         pointHoverBorderWidth: 2,
         pointRadius: 2,
         pointHitRadius: 10,
-        data: numOfWords,
+        data: numOfWords.swimming,
+      },
+    ],
+  };
+
+  const caloriesData = {
+    labels: dates.calories,
+    datasets: [
+      {
+        label: 'Затраченные каллории',
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(77,77, 77,0.47)',
+        borderColor: 'rgba(77,77, 77,0.47)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: '#000000',
+        pointBackgroundColor: '#000000',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgb(236,9,9)',
+        pointHoverBorderColor: 'rgb(236,9,9)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 2,
+        pointHitRadius: 10,
+        data: numOfWords.calories,
       },
     ],
   };
@@ -85,6 +132,8 @@ const StatisticLongTerm = (props) => {
         <Col lg={6}>
           <div className="statistic-long-term">
             <Line key="num" data={runningData} />
+            <Line key="num" data={swimmingData} />
+            <Line key="num" data={caloriesData} />
           </div>
         </Col>
       </Row>
